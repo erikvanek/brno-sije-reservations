734c91d767271547cbbe2e36f8781a95d51a630a
vanekerik <vanek.erik@gmail.com>
2020-02-14 00:06:07 +0100

    moving runs to dropdowns

--- src/Course.jsx
+++ src/Course.jsx
@@ -1,54 +1,68 @@
-import React from 'react';
-import { Runs } from './Runs';
+import React from "react";
+import { Runs } from "./Runs";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export class Course extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            course: undefined
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      course: undefined,
+      selectedRunId: undefined
+    };
+  }
 
-    fetchMyData(props) {
-        const courseUrl = props.match.params.url;
-        fetch(`http://localhost:3000/kurzy/${courseUrl}`)
-            .then(res => res.json())
-            .then(
-                result => {
-                    this.setState({
-                        course: result.course,
-                        reservations: result.reservations
-                    });
-                },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-                error => {
-                    console.error(error);
-                }
-            );
-    }
+  fetchMyData(props) {
+    const courseUrl = props.match.params.url;
+    fetch(`http://localhost:3000/kurzy/${courseUrl}`)
+      .then(res => res.json())
+      .then(
+        result => {
+          this.setState({
+            course: result.course,
+            reservations: result.reservations
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        error => {
+          console.error(error);
+        }
+      );
+  }
 
-    componentWillReceiveProps(nextProps) {
-        this.fetchMyData(nextProps);
-    }
+  componentWillReceiveProps(nextProps) {
+    this.fetchMyData(nextProps);
+  }
 
-    componentDidMount() {
-        this.fetchMyData(this.props);
-    }
+  componentDidMount() {
+    this.fetchMyData(this.props);
+  }
 
-    render() {
-        const { course, reservations } = this.state;
-        return (
-            <div>
-                <Link to="/">Seznam kurzů</Link>
-                <h2>{course && course.name}</h2>
-                {course && (
-                    <Runs course={course} reservations={reservations}></Runs>
-                )}
-            </div>
-        );
-    }
+  render() {
+    const { course, reservations } = this.state;
+    return (
+      <div>
+        <Link to="/">Seznam kurzů</Link>
+        <h2>{course && course.name}</h2>
+        {course && (
+          <div>
+            <Runs course={course} reservations={reservations} runChanged={(run) => this.runChanged(run)} buyCourse={this.buyCourse.bind(this)}></Runs>
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  runChanged(run) {
+    this.setState({...this.state, selectedRunId: Number.parseInt(run.target.value, 10)});
+  }
+
+  buyCourse() {
+      // not ideal to rely on initial order
+    const run = this.state.selectedRunId ? this.state.course.runs.find(run => run.id === this.state.selectedRunId) : this.state.course.runs[0];
+    console.log(run)
+  }
+
 }
--- src/Runs.jsx
+++ src/Runs.jsx
@@ -1,34 +1,26 @@
 import React from 'react';
 
-export function Runs(props) {
-    const hasRuns = props.course.runs;
+export class Runs extends React.Component {
+    render() {
+        const props = this.props;
+        const hasRuns = props.course.runs;
     return (
         <div>
             {hasRuns && (
                 <div>
                     <h3>Termíny:</h3>
-                    <ul>{props.course.runs.map(run => (
-                        <li key={run.id}>
-                            <span>
-                                {run.start} - {run.end}
-                            </span>
-                            <br />
-                            {props.course && (
-                                <span>
-                                    Volna mista:{' '}
-                                    {props.course.size -
-                                        props.reservations.filter(
+                    <select onChange={(course) => props.runChanged(course)}>
+                        {props.course.runs.map(run => (<option value={run.id} key={run.id}>{run.start} - {run.end}; {props.course.size - props.reservations.filter(
                                             reservation =>
                                                 reservation.runId === run.id
-                                        ).length}
-                                </span>
-                            )}
-                        </li>
-                        ))}
-                    </ul>
+                                        ).length} volných míst</option>))}
+                    </select>
+                    <button onClick={() => props.buyCourse()}>Zakoupit kurz</button>
                 </div>
             )}
             {!hasRuns && <h4>Pro tento kurz nejsou aktuálně k dispozici žádné volné termíny.</h4>}
         </div>
     );
+}
+
 }
